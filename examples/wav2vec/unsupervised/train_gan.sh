39d9c73e4ed0d08ec9eb07560469a874c06fe175
darongliu <givebirthday.repeat@gmail.com>
2022-09-23 01:05:50 +0800

    update train_gan.sh

--- examples/wav2vec/unsupervised/train_gan.sh
+++ examples/wav2vec/unsupervised/train_gan.sh
@@ -18,12 +18,16 @@ KENLM_PATH=/home/darong/frequent_data/wav2vecu/timit_processed/matched/phones/tr
 #     model.code_penalty=2,4 model.gradient_penalty=1.5,2.0 \
 #     model.smoothness_weight=0.5,0.75,1.0 'common.seed=range(0,5)'
 
+EXP_NAME=test
+
 PYTHONPATH=$FAIRSEQ_ROOT PREFIX=$PREFIX fairseq-hydra-train \
     -m --config-dir config/gan \
     --config-name $CONFIG_NAME \
+    hydra.sweep.dir=multirun/${EXP_NAME} \
     task.data=${TASK_DATA} \
     task.text_data=${TEXT_DATA} \
     task.kenlm_path=${KENLM_PATH} \
     common.user_dir=${FAIRSEQ_ROOT}/examples/wav2vec/unsupervised \
     model.code_penalty=2 model.gradient_penalty=1.5 \
-    model.smoothness_weight=0.5 common.seed=0
+    model.smoothness_weight=0.5 common.seed=0 \
+    optimization.max_update=100000 
