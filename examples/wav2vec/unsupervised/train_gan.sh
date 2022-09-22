0ba9a30ba09b244e999ff8441e6c4ad2d2b52666
darongliu <givebirthday.repeat@gmail.com>
2022-09-23 00:52:44 +0800

    update train_gan.sh

--- examples/wav2vec/unsupervised/train_gan.sh
+++ examples/wav2vec/unsupervised/train_gan.sh
@@ -8,6 +8,16 @@ TASK_DATA=/home/darong/frequent_data/wav2vecu/timit_processed/matched/feat/preco
 TEXT_DATA=/home/darong/frequent_data/wav2vecu/timit_processed/matched/phones  # path to fairseq-preprocessed GAN data (phones dir)
 KENLM_PATH=/home/darong/frequent_data/wav2vecu/timit_processed/matched/phones/train_text_phn.04.bin  # KenLM 4-gram phoneme language model (LM data = GAN data here)
 
+# PYTHONPATH=$FAIRSEQ_ROOT PREFIX=$PREFIX fairseq-hydra-train \
+#     -m --config-dir config/gan \
+#     --config-name $CONFIG_NAME \
+#     task.data=${TASK_DATA} \
+#     task.text_data=${TEXT_DATA} \
+#     task.kenlm_path=${KENLM_PATH} \
+#     common.user_dir=${FAIRSEQ_ROOT}/examples/wav2vec/unsupervised \
+#     model.code_penalty=2,4 model.gradient_penalty=1.5,2.0 \
+#     model.smoothness_weight=0.5,0.75,1.0 'common.seed=range(0,5)'
+
 PYTHONPATH=$FAIRSEQ_ROOT PREFIX=$PREFIX fairseq-hydra-train \
     -m --config-dir config/gan \
     --config-name $CONFIG_NAME \
@@ -15,5 +25,5 @@ PYTHONPATH=$FAIRSEQ_ROOT PREFIX=$PREFIX fairseq-hydra-train \
     task.text_data=${TEXT_DATA} \
     task.kenlm_path=${KENLM_PATH} \
     common.user_dir=${FAIRSEQ_ROOT}/examples/wav2vec/unsupervised \
-    model.code_penalty=2,4 model.gradient_penalty=1.5,2.0 \
-    model.smoothness_weight=0.5,0.75,1.0 'common.seed=range(0,5)'
+    model.code_penalty=2 model.gradient_penalty=1.5 \
+    model.smoothness_weight=0.5 common.seed=0
